f69458ab078b707e860c6dd896eee0e98312940e
Afrancisfinney <afrancisfinney647@students.fairmontschools.com>
2024-12-15 14:24:44 -0800

    Updated

--- Forestmanagement.java
+++ Forestmanagement.java
@@ -21,17 +21,38 @@ public class Forestmanagement {
         } else {
             System.out.println("Animals in the forest:");
             for (Animal animal : animals) {
-                System.out.println("- " + animal.getType());
+                System.out.println("- " + animal.getType()); 
             }
-
-            
-            for (int i = 0; i < animals.size() - 1; i++) {
-                for (int j = i + 1; j < animals.size(); j++) {
-                    Animal animal1 = animals.get(i);
-                    Animal animal2 = animals.get(j);
-                    System.out.println(animal1.compareSize(animal2));
+    
+            System.out.println("\n--- Animal Details ---");
+            for (Animal animal : animals) {
+                if (animal instanceof Lion) {
+                    System.out.println(((Lion) animal).getDetails());
+                } else if (animal instanceof Panther) {
+                    System.out.println(((Panther) animal).getDetails());
+                } else if (animal instanceof Tiger) {
+                    System.out.println(((Tiger) animal).getDetails());
+                } else if (animal instanceof Cheetah) {
+                    System.out.println(((Cheetah) animal).getDetails());
+                }
+            }
+    
+            System.out.println("\n--- Animal Size Comparisons ---");
+            if (animals.size() == 1) {
+                System.out.println("There is only one animal in the forest.");
+            } else {
+                for (int i = 0; i < animals.size() - 1; i++) {
+                    for (int j = i + 1; j < animals.size(); j++) {
+                        Animal animal1 = animals.get(i);
+                        Animal animal2 = animals.get(j);
+                        System.out.println(animal1.compareSize(animal2));
+                    }
                 }
             }
         }
     }
+    
+    
+    
+    
 }
